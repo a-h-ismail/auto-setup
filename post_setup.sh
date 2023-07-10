1e287f7a5c68f9f522a6d6a631e3e466730fb977
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2023-07-10 11:28:12 +0300

    Add running as root check.

--- post_setup.sh
+++ post_setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Script to execute after the complete installation
 # Possible use: clean up temporary files
+if [ $EUID -ne 0 ]; then
+    echo "You must run this as root, try with sudo."
+    exit 1
+fi
+
 echo 'Post setup script started.'
--- pre_setup.sh
+++ pre_setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Add commands to execute before the main setup script
 # For example: use to setup additional repositories.
+if [ $EUID -ne 0 ]; then
+    echo "You must run this as root, try with sudo."
+    exit 1
+fi
+
 echo 'Pre setup script started.'
